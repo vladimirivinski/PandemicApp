74630213a35741f4fdb5b0c7b50004d392cadc7f
Vladimir <vladimir.ivinski89@gmail.com>
2022-07-04 14:37:20 +0300

    upd

--- force-app/main/default/lwc/va_CarViewer/va_CarViewer.js
+++ force-app/main/default/lwc/va_CarViewer/va_CarViewer.js
@@ -1,25 +1,24 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement } from 'lwc';
 import compareDependentPicklistvalues from "@salesforce/apex/AV_CarsController.compareDependentPicklistvalues";
 
 export default class Va_CarViewer extends LightningElement {
     brandValue;
     modelValue;
-    @track _brands = [];
-    @track _models = [];
+     _brands = [];
+     _models = [];
 
     //=====================GETTERS========================================
     get brands(){
         return this._brands;
     }
     get models(){
-        return select;
+        return _models;
     }
     //====================================================================
 
     //=====================Callback=======================================
     async connectedCallback(){
         await this.getBrandsInfo();
-        await this.getModelInfo();
     }
     //====================================================================
 
@@ -28,11 +27,7 @@ export default class Va_CarViewer extends LightningElement {
         this._brands = await compareDependentPicklistvalues();
         console.log('this._brands:', this._brands);
     }
-    async getModelInfo(){
-        const selected = this._brands.find(e => e.value === this.brandValue);
-        
-        debugger
-        console.log('selected:', selected);
+    getModelInfo(){
         
     }
     //====================================================================
@@ -40,6 +35,7 @@ export default class Va_CarViewer extends LightningElement {
     //========================HANDLERS====================================
     handleBrandChange(event) {
         this.brandValue = event.detail.value;
+        console.log('this.brandValue:', this.brandValue);
     }
     handleModelChange(event) {
         this.modelValue = event.detail.value;
